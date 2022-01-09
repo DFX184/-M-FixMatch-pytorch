0ab1fbe978ed7fc57da1984400433587d539ddfc
David <59595277+DFX184@users.noreply.github.com>
2022-01-09 09:52:42 +0800

    Add files via upload

---
+++ utils/tool.py
@@ -0,0 +1,122 @@
+import torch
+import numpy as np
+import torch.nn as nn
+from torch.nn import init
+
+import math
+from torch.optim.lr_scheduler import LambdaLR
+
+def get_cosine_schedule_with_warmup(optimizer,
+                                    num_warmup_steps,
+                                    num_training_steps,
+                                    num_cycles=7./16.,
+                                    last_epoch=-1):
+    def _lr_lambda(current_step):
+        if current_step < num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+        no_progress = float(current_step - num_warmup_steps) / \
+            float(max(1, num_training_steps - num_warmup_steps))
+        return max(0., math.cos(math.pi * num_cycles * no_progress))
+
+    return LambdaLR(optimizer, _lr_lambda, last_epoch)    
+def linear_rampup(epochs):
+    def warpper(current):
+        if epochs == 0:
+            return 1.0
+        else:
+            current = np.clip(current / epochs, 0.0, 1.0)
+            return float(current)
+    return warpper
+
+def init_weight(m):
+    if isinstance(m, nn.Conv2d):
+        nn.init.xavier_uniform_(m.weight)
+    elif isinstance(m, nn.BatchNorm2d):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight)
+        nn.init.constant_(m.bias, 0)
+        
+        
+class AverageMeter(object):
+    """Computes and stores the average and current value
+       Imported from https://github.com/pytorch/examples/blob/master/imagenet/main.py#L247-L262
+    """
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        
+def mixup_one_target(x, y, alpha=1.0, device='cuda', is_bias=False):
+    """
+    Returns mixed inputs, mixed targets, and lambda
+    href : https://github.com/iBelieveCJM/Tricks-of-Semi-supervisedDeepLeanring-Pytorch
+    """
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+    if is_bias: lam = max(lam, 1-lam)
+
+    index = torch.randperm(x.size(0)).to(device)
+
+    mixed_x = lam*x + (1-lam)*x[index, :]
+    mixed_y = lam*y + (1-lam)*y[index]
+    return mixed_x, mixed_y, lam
+
+def exp_rampup(rampup_length):
+    """Exponential rampup from https://arxiv.org/abs/1610.02242"""
+    def warpper(epoch):
+        if epoch < rampup_length:
+            epoch = np.clip(epoch, 0.0, rampup_length)
+            phase = 1.0 - epoch / rampup_length
+            return float(np.exp(-5.0 * phase * phase))
+        else:
+            return 1.0
+    return warpper
+
+
+def interleave_offsets(batch, nu):
+    groups = [batch // (nu + 1)] * (nu + 1)
+    for x in range(batch - sum(groups)):
+        groups[-x - 1] += 1
+    offsets = [0]
+    for g in groups:
+        offsets.append(offsets[-1] + g)
+    assert offsets[-1] == batch
+    return offsets
+
+
+def interleave(xy, batch):
+    nu = len(xy) - 1
+    offsets = interleave_offsets(batch, nu)
+    xy = [[v[offsets[p]:offsets[p + 1]] for p in range(nu + 1)] for v in xy]
+    for i in range(1, nu + 1):
+        xy[0][i], xy[i][i] = xy[i][i], xy[0][i]
+    return [torch.cat(v, dim=0) for v in xy]
+
+def KL_div(P,Q):
+    kl_pq = -torch.mean(P * torch.log(Q/P))
+    #kl_qp = -torch.sum(Q * torch.log(P/Q))
+    #return (kl_qp + kl_pq)/2.0
+    return kl_pq
+
+def fix_interleave(x, size):
+    s = list(x.shape)
+    return x.reshape([-1, size] + s[1:]).transpose(0, 1).reshape([-1] + s[1:])
+
+
+def de_fix_interleave(x, size):
+    s = list(x.shape)
+    return x.reshape([size, -1] + s[1:]).transpose(0, 1).reshape([-1] + s[1:])
